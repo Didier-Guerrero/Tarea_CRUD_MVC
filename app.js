06fbdea38cd6fc674d37e7eaeef72ec5773659db
Didier Guerrero <didier.guerrero@udla.edu.ec>
2024-10-06 12:07:36 -0500

    Modificacion en el app.js final login

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const sequelize = require("./config/database");
 const itemRoutes = require("./routes/itemRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -13,11 +15,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Sincronizacion de la base de datos
 sequelize
   .sync({ force: false })
-  .then(() => console.log("Database synced"))
-  .catch((err) => console.log(err));
+  .then(() => console.log("Base de datos sincronizada"))
+  .catch((err) => console.log("Error al sincronizar la base de datos:", err));
 
 // Enrutamiento
 app.use("/", itemRoutes);
+app.use("/", authRoutes);
+
+app.use(
+  session({
+    secret: "mySecretKey",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
 
 const PORT = 3000;
 app.listen(PORT, () => {
